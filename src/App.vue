37cfdfe66d97fb21aa514289910ab736f13b1ec2
rakhmadi <rakhmadiwalker@gmail.com>
2021-11-16 05:40:44 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="main" class="flex justify-ceter">
+   <div class="block w-full md:w-1/3 bg-gray-100 h-full p-6 m-auto">
+     <router-view></router-view>
+   </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+@font-face {
+  font-family: 'Poppins';
+  src: url('./assets/Poppins-Regular.ttf');
+}
+  html,body{
+    width: 100%;
+    height: 100%;
+    font-family:'Poppins' !important;
+  }
+  #main,#app{
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-3 bg-white w-full rounded-md ">
+        <h1 class="text-2xl mb-4 mt-2 text-gray-600 text-center">Masukan Nama</h1>
+        <input type="text" placeholder="Masukan namamu di sini" class="px-2 py-2 border-purple-50 transition border-2 rounded-md w-full mb-2 focus:border-purple-300 focus:outline-none">
+        <button class="font-medium w-full px-4 py-2 rounded-md text-sm font-medium border-0 transition text-purple-600 bg-purple-50 hover:text-purple-800 hover:bg-purple-100 active:bg-purple-200  focus:outline-none " type="submit">Lanjut</button> 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
