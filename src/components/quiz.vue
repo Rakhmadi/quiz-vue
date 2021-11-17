784d08631b8f87f55a8930bc989e089ee6c1d748
rakhmadi <rakhmadiwalker@gmail.com>
2021-11-17 09:34:12 +0700

    commit

---
+++ src/components/quiz.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="p-3 bg-white w-full rounded-md" data-aos="fade-up">
+            a
+        </div>
+        <div class="p-3 bg-white w-full rounded-md mt-2">
+            <h1 class="text-2xl text-gray-600 mb-4 mt-2 text-center">1. Hai Klik untuk memulai</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    mounted() {
+        this.countDownTimer()
+        clearInterval(this.interval);
+        this.countDownTimer()
+ 
+        var countdownNum = localStorage.getItem('countdownNum');
+        if(!countdownNum){
+            localStorage.setItem('countdownNum',this.countDown)
+        }else{
+            this.countDown = localStorage.getItem('countdownNum')
+        }
+    },
+    data() {
+        return {
+            countDown : 20,
+        }
+    },
+    methods: {
+            countDownTimer() {
+                this.interval = setInterval(() => {
+                    if (this.countDown > 0) {
+                        this.countDown = this.countDown - 1;
+                        localStorage.setItem('countdownNum',this.countDown)
+                    } else {
+                        clearInterval(this.interval);
+                    }
+                }, 1000);
+            },
+    },
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import { createRouter,createWebHashHistory } from "vue-router";
 import Home from './components/Home.vue'
 import StartView from './components/StartView.vue'
+import quiz from './components/quiz.vue'
+
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
         { path: '/', component: Home },
         { path: '/StartView', component: StartView },
+        { path: '/quiz', component: quiz },
     ]
 })
 
