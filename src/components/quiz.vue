20ebf5284b8b8abcb4de2fffe3ebc77c8c23f33a
rakhmadi <rakhmadiwalker@gmail.com>
2021-11-19 16:24:51 +0700

    push

--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -6,8 +6,8 @@
                         <img class="w-14 h-14 rounded-full " src="https://avatarfiles.alphacoders.com/457/45744.png" alt="" srcset="">
                     </div>
                     <div class="self-center">
-                         <div class="rounded-full bg-green-100 h-14 w-14 flex justify-center">
-                             <font class="text-xl text-green-500 self-center">{{countDown}}s</font>
+                         <div  :class="{'text-yellow-500 ':true,'bg-yellow-500 ':true }" class=" rounded-full h-14 w-14 flex justify-center bg-green-100 text-green-500" >
+                             <font  class="text-xl self-center">{{countDown}}s</font>
                          </div>
                     </div>
                 </div>
@@ -23,13 +23,16 @@
                   </div>
             </div>
         </div>
-        <div class="p-3 bg-white w-full rounded-md mt-2">
-            <p class="text-xl text-gray-600 mb-2 mt-2 text-center">{{this.soalSinggle.soal}}</p>
-            <div class="flex flex-col p-2">
-                <div :class="{'border-purple-600':this.indexJawaban === index ,'text-purple-600':this.indexJawaban === index}" @click="selectOptions(index)" v-for="(item,index) in this.soalSinggle.jawaban" :key="index"  class="rounded-full w-full my-1 bg-purple-200 transition py-1 px-2 border-2 border-purple-300 text-purple-400 cursor-pointer">
-                    <font>{{item.text}}</font>
-                </div>
+        <div class="p-3 bg-white w-full rounded-md mt-2" data-aos="fade">
+            <div data-aos="fade">
+                 <p class="text-xl text-gray-600 mb-2 mt-2 text-center" data-aos="fade">{{this.soalSinggle.soal}}</p>
+                 <div class="flex flex-col p-2">
+                     <div :class="{'border-purple-600':this.indexJawaban === index ,'text-purple-600':this.indexJawaban === index}" @click="selectOptions(index, [this.soalSinggle.jawaban[index]])" v-for="(item,index) in this.soalSinggle.jawaban" :key="index"  class="rounded-full w-full my-1 bg-purple-200 transition py-1 px-2 border-2 border-purple-300 text-purple-400 cursor-pointer" >
+                         <font>{{item.text}}</font>
+                     </div>
+                 </div>
             </div>
+ 
             <div class="flex p-2 justify-end">
                 <button @click="goNext(),goPercent()" class="rounded-full bg-purple-300 py-1 px-2 border-2 border-purple-800 text-purple-800"><i class=" mdi mdi-arrow-right"></i> Next</button>
             </div>
@@ -60,11 +63,11 @@ export default {
     },
     data() {
         return {
-            
             page:localStorage.getItem('page'),
             countDown : 20,
             alphabets:["a","b","c","d","e"],
             indexJawaban:'',
+            jawabanSoal:[{"text":"tidak di jawab","correct":false}],
             soalSinggle : [],
             percent : 0,
             listSoal:[
@@ -108,9 +111,9 @@ export default {
     },
     methods: {
             goPercent(){
-                let n =localStorage.getItem('page')
+
                 let kali =  (localStorage.getItem('page')) * 100
-                this.percent = ((n <= 1)?n*100:n*100) / this.listSoal.length
+                this.percent = (kali) / this.listSoal.length
             },
             countDownTimer() {
                 this.interval = setInterval(() => {
@@ -121,12 +124,14 @@ export default {
                         clearInterval(this.interval);
                         this.goNext()
                         this.goPercent()
-
                     }
                 }, 1000);
             },
-            selectOptions(index){
-                this.indexJawaban = index 
+            selectOptions(index,jawaban){
+                this.indexJawaban = index
+                this.jawabanSoal = jawaban 
+                console.log(this.jawabanSoal);
+x
             },
             goNext(){
                 if(localStorage.getItem('page') >= this.listSoal.length){
@@ -134,13 +139,28 @@ export default {
                         localStorage.removeItem('page');
                         clearInterval(this.interval);
                         this.countDown = 20
+                        this.saverLocalstorage()
+                        this.jawabanSoal = []
+                        this.indexJawaban = ''
                 }else{
                         localStorage.setItem('page',Number(localStorage.getItem('page'))+1)
                         clearInterval(this.interval);
                         this.soalSinggle = this.listSoal[localStorage.getItem('page')-1]
                         this.countDown = 20
                         this.countDownTimer()
+                        this.saverLocalstorage()
+                        this.indexJawaban = ''
                 }
+            },saverLocalstorage(){
+                let getDataSoal = localStorage.getItem('dataSoal');
+
+                if(getDataSoal === null){
+                    localStorage.setItem('dataSoal',JSON.stringify([...this.jawabanSoal]))
+                  }else{
+                    let xN = JSON.parse(getDataSoal);
+                    localStorage.setItem('dataSoal',JSON.stringify([...xN,...this.jawabanSoal]));
+                  }
+                this.jawabanSoal = [{"text":"tidak di jawab","correct":false}]
             }
     },
 }
