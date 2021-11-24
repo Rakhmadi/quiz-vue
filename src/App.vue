781a6aea37ac54ebeb2ab5be05e5498b68a75a57
rakhmadi <rakhmadiwalker@gmail.com>
2021-11-24 17:15:42 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="main" class="h-full">
    <div class="w-full md:w-1/3 bg-gray-100 m-auto overflow-auto">
      <router-view></router-view>
+     <p class="text-xs text-gray-500 m-2">Designed by Rakhmadi with tailwindcss,vueJS and firestore</p>
    </div>
   </div>
 </template>
