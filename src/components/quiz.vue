f0e1605aea9994419a26d5ca6ae1fedef5cdf10d
rakhmadi <rakhmadiwalker@gmail.com>
2021-11-28 13:49:52 +0700

    update

--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -45,8 +45,18 @@
             </div>
  
             <div class="flex p-2 justify-end" v-show="this.indexJawaban != '' || this.indexJawaban === 0">
-                <button @click="goNext(),goPercent()" 
-                    class="rounded-full bg-purple-300 py-1 px-2 border-2 border-purple-800 text-purple-800">Next</button>
+                <button @click="goNext(),goPercent()" class="rounded-full bg-purple-300 py-1 px-2 border-2 border-purple-800 text-purple-800">
+                    <div class="flex flex-row">
+                        <div>
+                            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z" />
+                            </svg>
+                        </div>
+                        <div>
+                            Next
+                        </div>
+                    </div>    
+                </button>
             </div>
 
         </div>
